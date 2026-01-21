c5093b925f190d00d3603035285e050b92d0c589
Torben <tkopplin@icloud.com>
2026-01-21 12:19:20 +0100

    Added workspace defaults and dirs.

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -21,6 +21,28 @@ vim.o.mouse = ""
 opt.startofline = false
 vim.o.equalalways = true
 
+-- Workspace options
+vim.opt.undofile = true
+vim.g.workspace_autocreate = 1
+vim.g.workspace_autosave = 0
+
+local data = vim.fn.stdpath("data")
+vim.opt.undodir = { data .. "/undo//" }
+vim.opt.directory = { data .. "/swap//" }
+vim.opt.backupdir = { data .. "/backup//" }
+vim.g.workspace_session_directory = data .. "/workspace/sessions"
+local dirs = {
+  data .. "/undo",
+  data .. "/swap",
+  data .. "/backup",
+  data .. "/workspace/sessions",
+}
+
+for _, dir in ipairs(dirs) do
+  if vim.fn.isdirectory(dir) == 0 then
+    vim.fn.mkdir(dir, "p")
+  end
+end
 
 -- Keeping it until I have decided if the plugin is significantly better.
 -- -- Basic popup behaviour
