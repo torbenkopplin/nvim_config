45120d886363a98a466b88f3df7a841ff47195bd
Torben <tkopplin@icloud.com>
2026-02-04 11:58:05 +0100

    remap mA-Z to ma-z and vice versa.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -73,3 +73,17 @@ end, { desc = "Insert blank lines below."})
 keymap('n', '<C->', function()
   insert_blank_lines(1)
 end, { desc = "Insert blank lines above."})
+
+-- Source - https://stackoverflow.com/a/77404322
+-- Posted by Romain Vincent
+-- Retrieved 2026-02-03, License - CC BY-SA 4.0
+-- Use lowercase for global marks and uppercase for local marks.
+local low = function(i) return string.char(97+i) end
+local upp = function(i) return string.char(65+i) end
+
+for i=0,25 do 
+  vim.keymap.set("n", "m"..low(i), "m"..upp(i))
+  vim.keymap.set("n", "m"..upp(i), "m"..low(i))
+  vim.keymap.set("n", "'"..low(i), "'"..upp(i))
+  vim.keymap.set("n", "'"..upp(i), "'"..low(i))
+end
