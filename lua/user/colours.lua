c1e8248ce6a9d23c534b4c8c21021f3b09c11e2d
Torben <tkopplin@icloud.com>
2026-02-25 22:17:40 +0100

    noirblaze colour adjustment.

--- lua/user/colours.lua
+++ lua/user/colours.lua
@@ -41,4 +41,5 @@ end
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 vim.api.nvim_set_hl(0, "@lsp.type.parameter", { link = "Hlargs" })
+vim.api.nvim_set_hl(0, "@function", { link = "Function" })
 
